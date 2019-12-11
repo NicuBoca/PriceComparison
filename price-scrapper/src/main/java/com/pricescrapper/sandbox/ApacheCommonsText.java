01161045ee94891f8beb31a9ef21330d3e255994
Nicu <mail.boca.nicolae@gmail.com>
2019-12-11 18:59:27 +0200

    actualizare test similaritate

--- price-scrapper/src/main/java/com/pricescrapper/sandbox/ApacheCommonsText.java
+++ price-scrapper/src/main/java/com/pricescrapper/sandbox/ApacheCommonsText.java
@@ -21,42 +21,34 @@ public class ApacheCommonsText {
         String[] arrOfStr1 = str1.split(" ");
         String[] arrOfStr2 = str2.split(" ");
 
-        double sim[] = new double[str1.length()];
-        double coresp[] = new double[str2.length()];
-        int count[] = new int[str1.length()];
+        double sim[] = new double[arrOfStr1.length];
+        String simStr[] = new String[arrOfStr1.length];
+        int count[] = new int[arrOfStr1.length];
 
-        int i=0;
-        for (String a : arrOfStr1) {
-            //System.out.println(a);
-            int j=0;
-            for (String b : arrOfStr2) {
-                //System.out.println(b);
-                double p = js.apply(a,b);
+        for (int i=0; i<arrOfStr1.length; i++) {
+            //System.out.println(arrOfStr1[i]);
+            for (int j=0; j<arrOfStr2.length; j++) {
+                //System.out.println(arrOfStr2[j]);
+                double p = js.apply(arrOfStr1[i],arrOfStr2[j]);
                 if(p>sim[i]) {
                     sim[i] = p;
-                    coresp[j] = i;
+                    simStr[i] = arrOfStr2[j];
                 }
                 if(p==1) {
                     sim[i] = p;
-                    coresp[j] = i;
+                    simStr[i] = arrOfStr2[j];
                     count[i]++;
                 }
-                j++;
             }
-            i++;
         }
-        for (int x=0; x<arrOfStr1.length; x++) {
-            if(sim[x]==1){
-                System.out.println(arrOfStr1[x] + " apare de " + count[x] + " ori");
+
+        for (int k=0; k<arrOfStr1.length; k++) {
+            if(sim[k]==1){
+                System.out.println(arrOfStr1[k] + " apare de " + count[k] + " ori");
             }
             else {
-                System.out.println(arrOfStr1[x] + " are scor de " + sim[x]);
-                System.out.print("similare pentru " + arrOfStr1[x] + ": ");
-                for(int y=0; y<arrOfStr2.length; y++) {
-                    if(coresp[y]==x) {
-                        System.out.print(arrOfStr2[y] + " ");
-                    }
-                }
+                System.out.println(arrOfStr1[k] + " are scor de " + sim[k]);
+                System.out.println("similar pentru " + arrOfStr1[k] + ": " + simStr[k]);
             }
         }
 
